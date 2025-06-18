82448d0fab04379a2263161920ea2162287b8c25
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-06-18 13:58:57 -0300

    feat: def-func1

---
+++ def-func.py
@@ -0,0 +1,7 @@
+def saudacao_com_tratamento(nome, tratamento="Sr."):
+    print(f"Olá, {tratamento} {nome}! Seja bem-vindo!")
+
+saudacao_com_tratamento("Carlos")
+
+saudacao_com_tratamento("Carlos", "Excelentíssimo")
+saudacao_com_tratamento(tratamento = "Podre de Ruim", nome= "Carlos")
